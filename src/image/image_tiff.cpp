006a79c69c48ef1ea3c29c89c8cc6a593b71d324
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2017-09-28 12:21:25 +0200

    Removed unused functions

--- src/image/image_tiff.cpp
+++ src/image/image_tiff.cpp
@@ -15,13 +15,6 @@ StringVector ImageTiffIO::getExtensions() const
     return {"tiff"};
 }
 
-
-void ImageTiffIO::read(const size_t index, em::Image &image)
-{
-
-}
-
-
 ImageTiffIO::~ImageTiffIO()
 {
 
@@ -35,11 +28,6 @@ void ImageTiffIO::readHeader()
 size_t ImageTiffIO::getHeaderSize() const
 {
     return 0; // FIXME
-}
-
-size_t ImageTiffIO::getPadSize() const
-{
-    return 0;
 }
 
 em::ImageIO* ImageTiffIO::create() const
