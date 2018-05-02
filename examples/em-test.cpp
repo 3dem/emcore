b4522e24356c6a71943c9f711bf36e9737687265
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2018-05-02 21:47:11 +0200

    Created simple main program for testing and removed unused programs

---
+++ examples/em-test.cpp
@@ -0,0 +1,15 @@
+// A simple program
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "em/base/error.h"
+#include "em/image/image.h"
+#include "em/base/type.h"
+#include  <map>
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
