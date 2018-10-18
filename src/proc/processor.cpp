b611dac765c00a6957f7c39523c737adc9e90847
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2018-10-18 17:17:41 +0200

    Added simple class to compute Stats

--- src/proc/processor.cpp
+++ src/proc/processor.cpp
@@ -100,3 +100,52 @@ void ImageMathProc::process(Image &image)
             THROW_ERROR("Unsupported operation.");
     } // switch
 } // function ImageMathProc.process
+
+
+// -------------- Stats Implementation ---------------------------
+
+void Stats::compute(const Array &array)
+{
+    //FIXME: Now using a copy of the array in float to simplify code
+    // this could be implemented at Type level and save the extra copy
+    // that is being used now
+    Array arrayFloat;
+    arrayFloat.copy(array, typeFloat);
+
+    auto dim = array.getDim();
+    auto const data = static_cast<const float *>(arrayFloat.getData());
+    auto size = dim.getSize();
+
+    min = max = data[0];
+    std = mean = 0;
+
+    for (size_t i = 0; i < size; ++i)
+    {
+        double v = data[i];
+        if (v < min)
+            min = v;
+        else if (v > max)
+            max = v;
+        mean += v;
+        std += v * v;
+    }
+
+    if (size > 1)
+    {
+        mean /= size;
+        std = std / size - mean * mean;
+        std *= size / (size - 1);
+
+        // Foreseeing numerical instabilities
+        std = sqrt(static_cast< double >(abs(std)));
+    }
+    else
+        std = 0;
+} // Stats.compute
+
+std::ostream& em::operator<< (std::ostream &ostrm, const Stats &s)
+{
+    ostrm << "min: " << s.min << " max: " << s.max
+          << " avg: " << s.mean << " std: " << s.std << " ";
+    return ostrm;
+}
