10afacf198fb43a9831e6d053ca5ce9ae55038a1
J.M. de la Rosa Trevin <delarosatrevin@gmail.com>
2019-09-29 11:42:16 +0200

    Update in conda recipe

--- conda/recipe/build.sh
+++ conda/recipe/build.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 # Remove 'build' content, if compiling from directory
-make ${SRC_DIR}/build && cd ${SRC_DIR}/build
-echo "Current directory"
-pwd
+mkdir -p ${SRC_DIR}/build && cd ${SRC_DIR}/build && rm -rf *
+
 cmake -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release \
       -DPYTHON_EXECUTABLE=${PYTHON} \
       -DBUILD_EXAMPLES=ON -DBUILD_DOCOPT=ON -DBUILD_TIFF=ON -DBUILD_PNG=ON -DBUILD_JPEG=ON -DBUILD_PYBIND=ON \
-      ${SRC_DIR}
+      ..
 make -j 8
 make install
 
