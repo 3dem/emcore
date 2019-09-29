8766bb808bd3a617bb47a779bd44f40e852b216f
J.M. de la Rosa Trevin <delarosatrevin@gmail.com>
2019-09-29 11:42:02 +0200

    Fixed script to install to conda env

--- conda/install_to_env.py
+++ conda/install_to_env.py
@@ -1,24 +1,27 @@
 ###!/usr/bin/env bash
 
-import os, sys
+import os
+import sys
 import site
 
-sitePackages = site.getsitepackages()[0]
-prefix = os.path.dirname(os.path.dirname(sys.executable))
+pathDict = {
+    'sitePackages': site.getsitepackages()[0],
+    'prefix': os.path.dirname(os.path.dirname(sys.executable))
+}
 
 def system(cmd):
     print(cmd) 
     os.system(cmd)
 
-system('cd build; rm -rf *')
+system('mkdir -p build && cd build && rm -rf *')
 os.chdir('build')
 system('cmake .. -DCMAKE_PREFIX_PATH=%(prefix)s -DCMAKE_INSTALL_PREFIX=%(prefix)s '
        '-DBUILD_TIFF=ON -DBUILD_PNG=ON -DBUILD_JPEG=ON '
        '-DBUILD_TESTS=ON -DBUILD_DOCS=ON -DBUILD_EXAMPLES=ON '
        '-DCMAKE_BUILD_TYPE=Debug -DBUILD_DOCOPT=ON -DBUILD_PYBIND=ON '
-       '-DCMAKE_FIND_ROOT_PATH=%(prefix)s' % locals())
+       '-DCMAKE_FIND_ROOT_PATH=%(prefix)s' % pathDict)
 system('make -j 5')
 system('make install')
-system('mv %(prefix)s/lib/em.so %(sitePackages)s/' % locals())
+system('mv %(prefix)s/lib/emcore.so %(sitePackages)s/' % pathDict)
 
 
