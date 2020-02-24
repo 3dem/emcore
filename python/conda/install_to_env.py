1f58035b9a542c9bb2557320b0f39b1590ac757c
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2020-02-24 16:30:56 +0100

    Re-organized how to build the python binding code

--- python/conda/install_to_env.py
+++ python/conda/install_to_env.py
@@ -16,12 +16,10 @@ def system(cmd):
 system('mkdir -p build && cd build && rm -rf *')
 os.chdir('build')
 system('cmake .. -DCMAKE_PREFIX_PATH=%(prefix)s -DCMAKE_INSTALL_PREFIX=%(prefix)s '
-       '-DBUILD_TIFF=ON -DBUILD_PNG=ON -DBUILD_JPEG=ON '
-       '-DBUILD_TESTS=ON -DBUILD_DOCS=ON -DBUILD_EXAMPLES=ON '
-       '-DCMAKE_BUILD_TYPE=Debug -DBUILD_DOCOPT=ON -DBUILD_PYBIND=ON '
        '-DCMAKE_FIND_ROOT_PATH=%(prefix)s' % pathDict)
 system('make -j 5')
 system('make install')
-system('mv %(prefix)s/lib/emcore.so %(sitePackages)s/' % pathDict)
+system('cd ../python; python setup.py install ')
+#system('mv %(prefix)s/lib/emcore.so %(sitePackages)s/' % pathDict)
 
 
--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,25 @@
+import os
 import sys
+from glob import glob
 
 from skbuild import setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+version = None
+with open(os.path.join(os.path.dirname(here), 'VERSION.txt')) as f:
+    version = f.readline()
+
 setup(
     name="emcore",
-    version="0.0.4",
-    description="emcore binding",
-    author='jmrt',
-    license="MIT",
+    version=version,
+    description="Python module to access functionality in the emcore library",
+    long_description="",
+    url="https://github.com/3dem/emcore",
+    author='J.M. De la Rosa Trevin',
+    author_email='delarosatrevin@scilifelab.se',  # Optional
+    keywords='electron-microscopy cryo-em structural-biology image-processing',
+    license="GPLv3",
     packages=['emcore']
        
 )
