5436cce378a958aa8cbaa40ec01ef0211843814c
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2020-02-24 16:29:42 +0100

    Add --info option to check tables, columns and number of rows

--- examples/em-table.cpp
+++ examples/em-table.cpp
@@ -8,6 +8,7 @@
 #include "emc/os/filesystem.h"
 #include "emc/base/image.h"
 #include "emc/base/table.h"
+#include "emc/base/timer.h"
 #include "emc/proc/program.h"
 #include "emc/proc/processor.h"
 
@@ -18,13 +19,16 @@ static const char USAGE[] =
         R"(em-table.
 
     Usage:
-      em-table <input> [<output>]
+      em-table <input> [<output>] [--info]
 
     Options:
       <input>       An input file of a metadata file.
       <output>      The output file
       -h --help     Show this screen.
       --version     Show version.
+      --info        Print information about the metadata files.
+                    For each table it will print table name, columns
+                    and number of elements in the table.
 )";
 
 
@@ -56,6 +60,37 @@ private:
 
 // ---------------------- Implementation -------------------------------
 
+/**
+ * Print info about the input metadata file:
+ *  Tables, labels, and number of elements.
+ * @param inputFn
+ */
+void metadataInfo(const std::string& inputFn)
+{
+    TableFile tio;
+    tio.open(inputFn);
+    std::cout << std::setw(10) << std::right << "Tables: " << std::endl;
+
+    auto tables = tio.getTableNames();
+    for (auto &name: tables)
+    {
+        Timer timer;
+        timer.tic();
+        Table t;
+        tio.read(name, t);
+        auto e = timer.elapsed();
+
+        std::cout << "    " << name << std::endl;
+        std::cout << "        columns: ";
+        for (auto &col: t.getColumns())
+            std::cout << " " << col.getName();
+        std::cout << std::endl
+                  << "        rows: " << t.getSize() << std::endl;
+        std::cout << "        elapsed: " << e.count() << " secs. " << std::endl;
+    }
+}
+
+
 void EmTableProgram::readArgs()
 {
     inputFn = "";
@@ -75,35 +110,41 @@ void EmTableProgram::readArgs()
     ASSERT_ERROR(!Path::exists(inputFn),
                  String::join({"Input path '", inputFn, "' does not exists!!!"}));
 
-    TableFile tio;
-    tio.open(inputFn);
-    std::cout << std::setw(10) << std::right << "Tables: " << std::endl;
-
-    auto tables = tio.getTableNames();
-    for (auto &name: tables)
-        std::cout << "       " << name << std::endl;
-
-    Table t;
-    if (inputTable.empty())
-        inputTable = tables[0];
-
-    std::cout << " Reading table: " << inputTable << std::endl;
-    tio.read(inputTable, t);
-
-    if (hasArg("<output>"))
-    {
-        outputFn = getArg("<output>");
-        std::cout << " Writing table: " << inputTable << " to: "
-                  << outputFn << std::endl;
-        auto tio2 = TableFile();
-        tio2.open(outputFn, File::TRUNCATE);
-        tio2.write(inputTable, t);
-    }
+    if (hasArg("--info"))
+        metadataInfo(inputFn);
     else
     {
-        std::cout << "  count: " << t.getSize() << std::endl;
-        std::cout << "DEBUG: " << std::endl << t << std::endl;
+        TableFile tio;
+        tio.open(inputFn);
+        std::cout << std::setw(10) << std::right << "Tables: " << std::endl;
+
+        auto tables = tio.getTableNames();
+        for (auto &name: tables)
+            std::cout << "       " << name << std::endl;
+
+        Table t;
+        if (inputTable.empty())
+            inputTable = tables[0];
+
+        std::cout << " Reading table: " << inputTable << std::endl;
+        tio.read(inputTable, t);
+
+        if (hasArg("<output>"))
+        {
+            outputFn = getArg("<output>");
+            std::cout << " Writing table: " << inputTable << " to: "
+                      << outputFn << std::endl;
+            auto tio2 = TableFile();
+            tio2.open(outputFn, File::TRUNCATE);
+            tio2.write(inputTable, t);
+        }
+        else
+        {
+            std::cout << "  count: " << t.getSize() << std::endl;
+            std::cout << "DEBUG: " << std::endl << t << std::endl;
+        }
     }
+
 
 } // function EmTableProgram.readArgs
 
